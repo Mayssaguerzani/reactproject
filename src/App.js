6729e5d0683da00ab326268f3fafea097bc53efc
Your Name <you@example.com>
2021-11-30 11:44:26 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Form , Button} from 'react-bootstrap'
+import { Container , Col , Row} from 'react-bootstrap'
+import myImage from './mayssa.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container">
+      <Row>
+        <Col>
+      <h1>We Sell Flowers </h1>
+ <Form className="container">
+  <Form.Group className="mb-3 test" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" style={{width : "250px"}} />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" style={{width : "250px"}}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit" id="btn-submit">
+    Submit
+  </Button>
+</Form>
+</Col>
+<Col>
+<center>
+<img src= {myImage} alt="maysousa" style={{width : "400px" , height:"400px" ,borderRadius:"30%"}}/></center>
+</Col>
+</Row>
+
+    </Container>
   );
 }
 
